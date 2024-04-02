d330d930960d419e99c87282891b4b04bcfc08b0
Pranav Modgil <pranavmodgil@gmail.com>
2024-04-03 00:35:40 +0530

    Video 99

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,26 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+// Enum is custom data type that contains a fixed set of constants
+/* 
+  Enum below is used to represent the category of the expense.
+*/
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
-      : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4(); // Generation of random id by initialiser list
+  /*In Dart, "Initializer Lists" can be used to initialize class properties
+                          (like "id") with values that are NOT received as constructor
+                          function arguments.*/
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
