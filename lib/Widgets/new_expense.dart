b3dd9c79b6fc433a02096a93eff45307a75f5f35
Pranav Modgil <pranavmodgil@gmail.com>
2024-04-17 15:24:01 +0530

    Video 108 - Addind Button for using the Entered Value

--- lib/Widgets/new_expense.dart
+++ lib/Widgets/new_expense.dart
@@ -10,19 +10,36 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
+      // remove const in front of padding if error comes in future
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             // This is used to take the input from the user
             maxLength: 50,
             // keyboardType: TextInputType.name, // This is used to show the keyboard with the name type
-            decoration: InputDecoration(
-                // This is used to show the hint text in the text field
-                label: Text('Title')),
+            decoration: const InputDecoration(
+              // This is used to show the hint text in the text field
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_enteredTitle);
+                  },
+                  child: Text('Save Expense')),
+            ],
           )
         ],
       ),
