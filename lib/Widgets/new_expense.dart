7983f95cb4246b2dbe27f36e313361e0324394fa
Pranav Modgil <pranavmodgil@gmail.com>
2024-04-18 12:15:17 +0530

    Video 115 - Added Dropdown Button

--- lib/Widgets/new_expense.dart
+++ lib/Widgets/new_expense.dart
@@ -13,7 +13,7 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-
+  Category _selectedCategory = Category.leisure;
   DateTime? _selectedDate;
 
   void _presentDatePicker() async {
@@ -91,8 +91,31 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(
+              height: 16), // This is used to give space between the widgets
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
