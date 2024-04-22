2de630d6d49d00470d45adeb1be74535cd5855e0
Pranav Modgil <pranavmodgil@gmail.com>
2024-04-22 11:04:35 +0530

    Video 124

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/Widgets/expenses.dart';
 
+//k is used before variable name as a convention to represent global variables especially color scheme related.
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
+      home: const Expenses(),
       debugShowCheckedModeBanner: false,
     ),
   );
