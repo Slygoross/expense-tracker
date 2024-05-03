8d7c0f916b1d8a42b1503066c8ed1518561658b6
Pranav Modgil <pranavmodgil@gmail.com>
2024-05-03 14:59:18 +0530

    Video 138 - Removed overlapping in keyboard while adding new expense

--- lib/Widgets/new_expense.dart
+++ lib/Widgets/new_expense.dart
@@ -82,94 +82,102 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      // remove const in front of padding if error comes in future
-      padding: EdgeInsets.fromLTRB(16, 48, 16, 16),
-      child: Column(
-        children: [
-          TextField(
-            controller: _titleController,
-            // This is used to take the input from the user
-            maxLength: 50,
-            // keyboardType: TextInputType.name, // This is used to show the keyboard with the name type
-            decoration: const InputDecoration(
-              // This is used to show the hint text in the text field
-              label: Text('Title'),
-            ),
-          ),
-          Row(
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+    // Gives out the space that is taken up by anything overlapping from the bottom - in this case the keyboard.
+    return SizedBox(
+      // so that modal takes up entire space again
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+          // remove const in front of padding if error comes in future
+          padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
+          child: Column(
             children: [
-              Expanded(
-                child: TextField(
-                  controller: _amountController,
-                  // This is used to take the input from the user
-                  // maxLength: 10,
-                  keyboardType: TextInputType
-                      .number, // This is used to show the keyboard with the name type
-                  decoration: const InputDecoration(
-                    // This is used to show the hint text in the text field
-                    label: Text('Amount'),
-                    prefixText: '\₹ ',
-                  ),
+              TextField(
+                controller: _titleController,
+                // This is used to take the input from the user
+                maxLength: 50,
+                // keyboardType: TextInputType.name, // This is used to show the keyboard with the name type
+                decoration: const InputDecoration(
+                  // This is used to show the hint text in the text field
+                  label: Text('Title'),
                 ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _amountController,
+                      // This is used to take the input from the user
+                      // maxLength: 10,
+                      keyboardType: TextInputType
+                          .number, // This is used to show the keyboard with the name type
+                      decoration: const InputDecoration(
+                        // This is used to show the hint text in the text field
+                        label: Text('Amount'),
+                        prefixText: '\₹ ',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(_selectedDate == null
+                            ? 'No Date Selected'
+                            : formatter.format(_selectedDate!)),
+                        IconButton(
+                          onPressed: _presentDatePicker,
+                          icon: const Icon(Icons.calendar_month),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(
-                width: 16,
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(_selectedDate == null
-                        ? 'No Date Selected'
-                        : formatter.format(_selectedDate!)),
-                    IconButton(
-                      onPressed: _presentDatePicker,
-                      icon: const Icon(Icons.calendar_month),
-                    ),
-                  ],
-                ),
-              ),
+                  height: 16), // This is used to give space between the widgets
+              Row(
+                children: [
+                  DropdownButton(
+                      value: _selectedCategory,
+                      items: Category.values
+                          .map(
+                            (category) => DropdownMenuItem(
+                              value: category,
+                              child: Text(
+                                category.name.toUpperCase(),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        if (value == null) {
+                          return;
+                        }
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      }),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                      onPressed: _submitExpenseData,
+                      child: const Text('Save Expense')),
+                ],
+              )
             ],
           ),
-          const SizedBox(
-              height: 16), // This is used to give space between the widgets
-          Row(
-            children: [
-              DropdownButton(
-                  value: _selectedCategory,
-                  items: Category.values
-                      .map(
-                        (category) => DropdownMenuItem(
-                          value: category,
-                          child: Text(
-                            category.name.toUpperCase(),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                  onChanged: (value) {
-                    if (value == null) {
-                      return;
-                    }
-                    setState(() {
-                      _selectedCategory = value;
-                    });
-                  }),
-              const Spacer(),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Cancel'),
-              ),
-              ElevatedButton(
-                  onPressed: _submitExpenseData,
-                  child: const Text('Save Expense')),
-            ],
-          )
-        ],
+        ),
       ),
     );
   }
